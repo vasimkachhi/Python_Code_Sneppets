e75032f10b372b54506f56c72500c36b2a602a42
vasimkachhi <vasimkachhi@gmail.com>
2017-01-25 15:18:04 +0530

    Reading File python

--- readingFile.py
+++ readingFile.py
@@ -4,7 +4,7 @@
 """
 count = 0
 flag = True
-for index, line in enumerate(open("E:\\vasimk\\BACKUP\\BACKUP\\PythonPract\\test1.txt").readlines()):
+for index, line in enumerate(open("test.txt").readlines()):
     d = str(line)
     count += d.count("manila")
     if count >= 8 and flag:
@@ -13,17 +13,16 @@ for index, line in enumerate(open("E:\\vasimk\\BACKUP\\BACKUP\\PythonPract\\test
 
 
 import json
-a = json.loads(open("E:\\vasimk\\BACKUP\\BACKUP\\PythonPract\\test2.json").read()).keys()
-b = str(open("E:\\vasimk\\BACKUP\\BACKUP\\PythonPract\\test1.txt").read()).strip().split(",")
+a = json.loads(open("testjson.json").read()).keys()
+b = str(open("test.txt").read()).strip().split(",")
 print a
 print b
 print list(set(a).intersection(set(b)))
 
-print open("E:\\vasimk\\BACKUP\\BACKUP\\PythonPract\\test1.txt").readline(60)
+print open("test.txt").readline(60)
 
 
-x =  json.loads(open("E:\\vasimk\\BACKUP\\BACKUP\\PythonPract\\test2.json").read())
+x =  json.loads(open("testjson.json").read())
 y = json.dumps(x)
 print x
 print y
-
